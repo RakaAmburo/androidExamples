dfefe841ffe90c209aaed33d8933f71972a412a8
pablo <pablopaparini@hotmail.com>
2023-05-01 12:33:48 +0200

    First commit for unlock by udp branch android example.This branch will have improvements in the near future.

--- app/src/main/java/com/automate/loginapp/MainActivity.java
+++ app/src/main/java/com/automate/loginapp/MainActivity.java
@@ -1,14 +1,24 @@
 package com.automate.loginapp;
 
 import android.app.Activity;
+import android.content.Context;
+import android.net.DhcpInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.ebanx.swipebtn.SwipeButton;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 public class MainActivity extends Activity {
 
-    int s = 1;
+    private int lockState = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +27,12 @@ public class MainActivity extends Activity {
         SwipeButton swipeButton = findViewById(R.id.swipe);
 
         swipeButton.setOnStateChangeListener(active -> {
-            s = s + 1;
-            if (s % 2 == 0) {
-                Toast.makeText(MainActivity.this, "message sent", Toast.LENGTH_SHORT).show();
+            lockState = lockState + 1;
+            if (lockState % 2 == 0) {
+                String message = sendBroadcast("UNLOCK_MACHINE");
+                Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
                 try {
-                    Thread.sleep(3000);
+                    Thread.sleep(4000);
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
@@ -30,5 +41,36 @@ public class MainActivity extends Activity {
         });
     }
 
+    private String sendBroadcast(String messageStr) {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        String statusMessage;
+        try (DatagramSocket socket = new DatagramSocket()) {
+            socket.setBroadcast(true);
+            byte[] sendData = messageStr.getBytes();
+            InetAddress inetAddress = getBroadcastAddress();
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, inetAddress, 8286);
+            socket.send(sendPacket);
+            statusMessage = "Broadcast sent to: " + inetAddress.getHostAddress();
+        } catch (IOException e) {
+            statusMessage = "UDP broadcast error";
+            Log.e("UDP broadcast error: ", "IOException: " + e.getMessage());
+        }
+
+        return statusMessage;
+    }
+
+    private InetAddress getBroadcastAddress() throws IOException {
+        WifiManager wifi = (WifiManager)
+                this.getSystemService(Context.WIFI_SERVICE);
+        DhcpInfo dhcp = wifi.getDhcpInfo();
+
+
+        int broadcast = (dhcp.ipAddress & dhcp.netmask) | ~dhcp.netmask;
+        byte[] quads = new byte[4];
+        for (int k = 0; k < 4; k++)
+            quads[k] = (byte) ((broadcast >> k * 8) & 0xFF);
+        return InetAddress.getByAddress(quads);
+    }
 
 }
