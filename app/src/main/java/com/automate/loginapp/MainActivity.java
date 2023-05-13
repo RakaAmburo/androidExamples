9d09557b12b3554f1fdf91964b8f9e037c462f01
pablo <pablopaparini@hotmail.com>
2023-05-13 11:26:42 +0200

    Adding UDP to voice recog

--- app/src/main/java/com/automate/loginapp/MainActivity.java
+++ app/src/main/java/com/automate/loginapp/MainActivity.java
@@ -2,22 +2,32 @@ package com.automate.loginapp;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.DhcpInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.os.SystemClock;
 import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.util.ArrayList;
 
 public class MainActivity extends Activity implements RecognitionListener {
@@ -189,7 +199,45 @@ public class MainActivity extends Activity implements RecognitionListener {
             capturedVoiceCmd.setText("Listening Service Error");
         } else {
             capturedVoiceCmd.setText(matches.get(0));
+            if (matches.get(0).equalsIgnoreCase("computer unlock")){
+                String message = sendBroadcast("UNLOCK_MACHINE");
+                Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+            }
         }
+    }
+
+    private String sendBroadcast(String messageStr) {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        String statusMessage;
+        try (DatagramSocket socket = new DatagramSocket()) {
+            socket.setBroadcast(true);
+            byte[] sendData = messageStr.getBytes();
+            InetAddress inetAddress = getBroadcastAddress();
+            DatagramPacket sendPacket =
+                    new DatagramPacket(sendData, sendData.length, inetAddress, 8286);
+            socket.send(sendPacket);
+            statusMessage = "Broadcast sent to: " + inetAddress.getHostAddress();
+        } catch (IOException e) {
+            statusMessage = "UDP broadcast error";
+            Log.e("UDP broadcast error: ", "IOException: " + e.getMessage());
+        }
+
+        return statusMessage;
+    }
+
+    private InetAddress getBroadcastAddress() throws IOException {
+        WifiManager wifi = (WifiManager)
+                this.getSystemService(Context.WIFI_SERVICE);
+        DhcpInfo dhcp = wifi.getDhcpInfo();
+
+
+        int broadcast = (dhcp.ipAddress & dhcp.netmask) | ~dhcp.netmask;
+        byte[] quads = new byte[4];
+        for (int k = 0; k < 4; k++) {
+            quads[k] = (byte) ((broadcast >> k * 8) & 0xFF);
+        }
+        return InetAddress.getByAddress(quads);
     }
 
     @Override
