137cebb89ac06f5cc6675dea544b1c56ddfaaeb6
RakaAmburo <pablopaparini@hotmail.com>
2023-12-30 14:33:43 +0100

    changing id and app name

--- app/src/main/java/com/automate/loginapp/MainActivity.java
+++ app/src/main/java/com/automate/loginapp/MainActivity.java
@@ -8,66 +8,85 @@ import android.os.Bundle;
 import android.os.StrictMode;
 import android.util.Log;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
 import android.widget.Toast;
-import android.widget.ToggleButton;
-
-import com.andrognito.patternlockview.PatternLockView;
-import com.andrognito.patternlockview.listener.PatternLockViewListener;
-import com.andrognito.patternlockview.utils.PatternLockUtils;
-import com.ebanx.swipebtn.SwipeButton;
 
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.util.List;
 
 public class MainActivity extends Activity {
 
-    ToggleButton togglebutton;
-    TextView textview;
+    Button switch_1;
+    Button switch_2;
+    Button switch_3;
+    Button switch_4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        switch_1 = findViewById(R.id.switch_1);
+        switch_1.setText("On");
+        switch_2 = findViewById(R.id.switch_2);
+        switch_2.setText("On");
+        switch_3 = findViewById(R.id.switch_3);
+        switch_3.setText("On");
+        switch_4 = findViewById(R.id.switch_4);
+        switch_4.setText("On");
 
-        togglebutton
-                = (ToggleButton)findViewById(
-                R.id.toggleButton);
-
-        textview
-                = (TextView)findViewById(
-                R.id.textView);
 
     }
 
-    public void onToggleClick(View view)
+    public void onToggleClick(View v)
     {
-        if (togglebutton.isChecked()) {
-            textview.setText("Toggle is ON");
+
+        if (v.getId() == R.id.switch_1){
+            String response = sendBroadcast("SWITCH_1_OFF");
+            Toast.makeText(getApplicationContext(),response ,Toast.LENGTH_SHORT).show();
         }
-        else {
-            textview.setText("Toggle is OFF");
+        if (v.getId() == R.id.switch_2){
+            String response = sendBroadcast("SWITCH_2_OFF");
+            Toast.makeText(getApplicationContext(),response ,Toast.LENGTH_SHORT).show();
+        }
+        if (v.getId() == R.id.switch_3){
+            String response = sendBroadcast("SWITCH_3_OFF");
+            Toast.makeText(getApplicationContext(),response ,Toast.LENGTH_SHORT).show();
+        }
+        if (v.getId() == R.id.switch_4){
+            String response = sendBroadcast("SWITCH_4_OFF");
+            Toast.makeText(getApplicationContext(),response ,Toast.LENGTH_SHORT).show();
         }
     }
 
     private String sendBroadcast(String messageStr) {
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
-        String statusMessage;
+
+        String statusMessage = "EMPTY";
         try (DatagramSocket socket = new DatagramSocket()) {
             socket.setBroadcast(true);
             byte[] sendData = messageStr.getBytes();
             InetAddress inetAddress = getBroadcastAddress();
             DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, inetAddress, 8286);
             socket.send(sendPacket);
-            statusMessage = "Broadcast sent to: " + inetAddress.getHostAddress();
         } catch (IOException e) {
             statusMessage = "UDP broadcast error";
             Log.e("UDP broadcast error: ", "IOException: " + e.getMessage());
+            return statusMessage;
+        }
+
+        byte[] receiveByte = new byte[1024];
+        try (DatagramSocket receivingSocket = new DatagramSocket(8284)) {
+            DatagramPacket receivePack = new DatagramPacket(receiveByte, receiveByte.length);
+            receivingSocket.receive(receivePack);
+            String receiveStr = new String(receiveByte, receivePack.getOffset(), receivePack.getLength());
+            statusMessage = receiveStr;
+        } catch (IOException e) {
+            statusMessage = "UDP receive error";
+            Log.e("UDP receive error: ", "IOException: " + e.getMessage());
         }
 
         return statusMessage;
